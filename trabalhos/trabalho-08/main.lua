511c2df23dc2305235c39e9a4ec3bcef1d11d9f3
NiltonJunior <nilton.gjr@gmail.com>
2017-07-01 16:44:18 -0300

    Corrigido em 01/Jul/2017

--- trabalhos/trabalho-08/main.lua
+++ trabalhos/trabalho-08/main.lua
@@ -1,11 +1,18 @@
+
+--[[TRABALHO-08 os codigos se encontram da L 19 até a 44-- (coroutine) e das linhas com a anotação --TRABALHO-08 ]]
+
 function love.load ()
     p2 = { x=350, y=550, w=100, h=10 }
-	p3 = { x=400, y=250, w=10, h=10, vy=-190,vx = 1}
-	p4 = { x=350, y=250, w=10, h=10, vy=-50,vx = 50}
-	mov=1 --variavel da coroutine
+	p3 = { x=400, y=300, w=10, h=10, vy=-190,vx = 1}
+	p4 = { x=100, y=250, w=100, h=10, vy=-50,vx = 50}
 	bonus = {}
 	enhance = {}
 	quadrados=84
+	--TRABALHO-08 CRIA UM NOVO PONTO BONUS COM VALOR PRÓPRIO DE I PARA SER USADO NO CALCULO DO BONUS
+	for i=1,5 do
+		enhance[i] = pontoBonus()
+	end
+	
 	a = {}
     for i=1, 6 do
 		a[i]={}
@@ -13,42 +20,47 @@ function love.load ()
            a[i][j] = { x=j*50, y=i*30, w=30, h=10}
 		end
     end
+	
+	--TRABALHO-08 A coroutine move o quadrado toda vez que este for atingido pela bola (LINHA 196)
 	co = coroutine.create(function (dt)
         while true do
-			if mov>4 then
-				mov=1
+			for i=1,500 do
+				p4.x=100+i+p4.vx*dt
+				dt =coroutine.yield()
 			end
-			if mov==1 then
-				p4.x=p4.x+p4.vx*dt
-            end
-			if mov==2 then
-				p4.y=p4.y+p4.vy*dt
-            end
-			if mov==3 then
-				p4.x=p4.x-p4.vx*dt
-            end
-			if mov==4 then
-				p4.y=p4.y-p4.vy*dt
-            end
-			dt =coroutine.yield()
+
+			for i=1,200 do
+				p4.y=250+i+p4.vy*dt
+				dt =coroutine.yield()
+			end
+
+			for i=1,500 do
+				p4.x=600-i-p4.vx*dt
+				dt =coroutine.yield()
+			end
+
+			for i=1,200 do
+				p4.y=450-i-p4.vy*dt
+				dt =coroutine.yield()
+			end
         end
     end)
-
-	
 	
 	isPressed=0
 	val = 0
 	hit=0
 	exist=0
+	vab = 0
 
 end
 
 function pontoBonus () 
---[[TRABALHO-08 essa função será um plus de bonus do trabalho-07, 
-que aumenta a pontuação bonus conforme o jogo passa através de uma clousure, dependendo do tipo de bonus--]]
-    local i = 84-quadrados
-    return function ()   
-        i = i * math.floor(i/10)
+--[[TRABALHO-08 a COROUTINE funciona da seguinte forma: a cada acerto (hit) da bolinha nos blocos, é criado uma nova variável i com o valor de 
+(i*2). quando o bonus é chamado, o valor do bonus será um valor randomico entre essas variáveis.
+--]]
+    local i = 10
+    return function () 
+		i=i*2
 		return i
     end
 end
@@ -131,18 +143,27 @@ function love.update (dt)
 		end
 	end
 
+	if collides(p2, p3) then
+		if collidesX(p2,p3) then
+			p3.vx=(10*(p3.x-((p2.w/2)+p2.x)))
+			if ((p2.x+(p2.w/2)<p3.x+p3.w) and p3.vx<0) or ((p2.x+(p2.w/2)>p3.x+p3.w) and p3.vx>0) then
+				p3.vx=(-1)*p3.vx
+			end
+		elseif	collidesY(p2,p3) then
+			p3.vx=p3.vx*(-1)
+		end
+		p3.vy=p3.vy*(-1.01)
+		
+    end
+	
 	if exist==1 then
 		bonus.y = bonus.y + bonus.vy*0.2
 		if collides(p2,bonus) then
 			exist=0
 			hit=0
-			bonuses=0;
-			--chamada da clousure do TRABALHO-08
-			enhance[bonuses] = pontoBonus()
-			val= val + enhance[bonuses]()
-			bonuses=bonuses+1
 			erase()
-
+			--TRABALHO-08 INCREMENTA VAL DE ACORDO COM UM BONUS RANDOM DE VALOR DEFINIDO LOCALMENTE NA CLOSURE
+			val= val + vab
 		end
 	end
 	
@@ -174,20 +195,22 @@ function love.update (dt)
 		end
     end
 	
-    if collides(p2, p3) then
-		if collidesX(p2,p3) then
-			p3.vx=(10*(p3.x-((p2.w/2)+p2.x)))
-			if ((p2.x+(p2.w/2)<p3.x+p3.w) and p3.vx<0) or ((p2.x+(p2.w/2)>p3.x+p3.w) and p3.vx>0) then
+
+	
+	if collides(p4, p3) then
+		if collidesX(p4,p3) then
+			p3.vx=(10*(p3.x-((p4.w/2)+p4.x)))
+			if ((p4.x+(p4.w/2)<p3.x+p3.w) and p3.vx<0) or ((p4.x+(p4.w/2)>p3.x+p3.w) and p3.vx>0) then
 				p3.vx=(-1)*p3.vx
 			end
-		elseif	collidesY(p2,p3) then
+		elseif	collidesY(p4,p3) then
 			p3.vx=p3.vx*(-1)
 		end
 		p3.vy=p3.vy*(-1.01)
-		mov = mov+1 --AQUI QUANDO A BOLINHA ATINGE A BARRA MAIOR A COROUTINE MUDA POR CAUSA DO MOV.
+
     end
-	
-	coroutine.resume(co,dt) --COROUTINE DO JOGO PARA MOVER QUADRADO
+			coroutine.resume(co,dt) --TRABALHO-08 COROUTINE DO JOGO PARA MOVER QUADRADO
+
 
 	for i=1, 6 do
 		for j=1, 14 do
@@ -204,6 +227,13 @@ function love.update (dt)
 				a[i][j].h=0
 				val=val+1
 				quadrados=quadrados-1 
+				
+				if exist=0 then
+					--TRABALHO-08 a cada hit ele ja deixa criado o valor do bonus incrementando nas chamadas da closure
+					pos=math.random(1,5)
+					--TRABALHO-08 aloca valor de enhance ('i') em vab para usar em bonus 
+					vab = enhance[pos]()
+				end
 				if val % 7 ==0 and exist==0 then
 					hit=1
 					alloc(math.random(0,600),300,15,15,10)
@@ -220,7 +250,8 @@ function love.draw ()
 	love.graphics.rectangle('fill', p2.x,p2.y, p2.w,p2.h)
     love.graphics.rectangle('fill', p3.x,p3.y, p3.w,p3.h)
 	love.graphics.setColor(255, 255, 255, 255)
-    love.graphics.rectangle('fill', p4.x,p4.y, p4.w,p4.h)
+    --TRABALHO-08 quadrado da coroutine
+	love.graphics.rectangle('fill', p4.x,p4.y, p4.w,p4.h)
     love.graphics.print("Pontuacao: ".. val, 50, 570, 0, 1.2, 1.2)
 	love.graphics.print("Quadrados Restantes: ".. quadrados, 250, 570, 0, 1.2, 1.2)
 	love.graphics.print("Velocidade: ".. math.floor(p3.vy), 500, 570, 0, 1.2, 1.2)
@@ -235,7 +266,7 @@ function love.draw ()
 		exist=1
 		love.graphics.setColor(255, 255, 0, 255)
 		love.graphics.rectangle('fill', bonus.x,bonus.y, bonus.w,bonus.h)
-		love.graphics.print("PEGUE O QUADRADO DE BONUS:",250, 5, 0, 1.2, 1.2)
+		love.graphics.print("PEGUE O QUADRADO DE BONUS, ele vale: "..vab,250, 5, 0, 1.2, 1.2)
 
 	end
 
